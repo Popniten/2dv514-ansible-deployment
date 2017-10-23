4bf9f9422dee6c60f8cb7ba9607c04383a5997d9
Pär Eriksson <pe222gq@student.lnu.se>
2017-10-24 01:11:57 +0200

    Adding script.

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,10 +2,10 @@
 
 echo '. ~/Group02_project-openrc.sh' >> ~/.bashrc
 echo 'eval `ssh-agent -s`' >> ~/.bashrc
-echo 'chmod 700 ~/project/2dv514-g2-key.pem' >> ~/.bashrc
-echo 'ssh-add ~/project/2dv514-g2-key.pem' >> ~/.bashrc
+echo 'chmod 700 ~/2dv514-g2-key.pem' >> ~/.bashrc
+echo 'ssh-add ~/2dv514-g2-key.pem' >> ~/.bashrc
 
 . ~/Group02_project-openrc.sh
 eval `ssh-agent -s`
-chmod 700 ~/project/2dv514-g2-key.pem
-ssh-add ~/project/2dv514-g2-key.pem
+chmod 700 ~/2dv514-g2-key.pem
+ssh-add ~/2dv514-g2-key.pem
